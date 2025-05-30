24f300a7d32f9953358efb341e50323164d9a397
MWR27 <64335495+MWR27@users.noreply.github.com>
2025-05-30 09:54:53 -0400

    Initial commit

---
+++ collisions.py
@@ -0,0 +1,91 @@
+import random
+
+class Clusters:
+    def __init__(self, cluster_counts):
+        self._cluster_counts = cluster_counts.copy()
+        self._collision_count = 0
+        self._particle_count = 0
+        self._weights = {}
+        for cluster_size, cluster_count in self._cluster_counts.items():
+            total_particle_count = cluster_size * cluster_count
+            self._particle_count += total_particle_count
+            self._weights[cluster_size] = total_particle_count
+
+    @classmethod
+    def create_monomers(cls, count):
+        d = {1: count}
+        return cls(d)
+
+    def __str__(self):
+        return str(self._cluster_counts)
+
+    def particle_count(self):
+        return self._particle_count
+
+    def cluster_counts(self):
+        return self._cluster_counts.copy()
+
+    def cluster_count(self, cluster_size):
+        if cluster_size in self._cluster_counts:
+            return self._cluster_counts[cluster_size]
+        else:
+            return 0
+
+    def collision_count(self):
+        return self._collision_count
+
+    def collide(self, times, removals=0):
+        successful_collisions = 0
+        for i in range(times):
+            # check if there is only 1 or no clusters left
+            if (len(self._cluster_counts) == 1 and next(iter(self._cluster_counts.values())) == 1) or self.particle_count() == 0:
+                break
+            else:
+                # pick one cluster
+                a = random.choices(population=list(self._cluster_counts.keys()), weights=list(self._weights.values()))[0]
+                self.remove_cluster(a)
+                # pick another cluster
+                b = random.choices(population=list(self._cluster_counts.keys()), weights=list(self._weights.values()))[0]
+                self.remove_cluster(b)
+                if a + b - removals > 0:
+                    self.add_cluster(a + b - removals)
+                successful_collisions += 1
+        self._collision_count += successful_collisions
+        return successful_collisions
+
+    def add_cluster(self, cluster_size):
+        if cluster_size in self._cluster_counts:
+            self._cluster_counts[cluster_size] += 1
+            self._weights[cluster_size] += cluster_size
+        elif cluster_size > 0:
+            self._cluster_counts[cluster_size] = 1
+            self._weights[cluster_size] = cluster_size
+        else:
+            raise ValueError(f'cannot have cluster of size {cluster_size} in group of clusters')
+        self._particle_count += cluster_size
+
+    def remove_cluster(self, cluster_size):
+        if cluster_size in self._cluster_counts:
+            if self._cluster_counts[cluster_size] == 1:
+                del self._cluster_counts[cluster_size]
+                del self._weights[cluster_size]
+            else:
+                self._cluster_counts[cluster_size] -= 1
+                self._weights[cluster_size] -= cluster_size
+            self._particle_count -= cluster_size
+        else:
+            raise ValueError(f'no cluster of size {cluster_size} in group of clusters')
+
+
+# examples
+clusters_a = Clusters.create_monomers(100)
+print(f'Original (Size {clusters_a.particle_count()}):\n', clusters_a)
+successful_collisions = clusters_a.collide(50)
+print(f'After {successful_collisions} collisions (Size {clusters_a.particle_count()}):\n', clusters_a)
+print()
+
+clusters_b = Clusters({2: 100000})
+print(f'Original (Size {clusters_b.particle_count()}):\n', clusters_b)
+successful_collisions = clusters_b.collide(50000, 1)
+print(f'After {successful_collisions} collisions (Size {clusters_b.particle_count()}):\n', clusters_b)
+print('Size:', clusters_b.particle_count())
