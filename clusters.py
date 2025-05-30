79706192c156c3b589d00fd82e03bd508c1ffe12
Matthew Rakauskas <matthew.rakauskas@scranton.edu>
2025-05-30 12:44:00 -0400

    Separate script and Clusters class

--- clusters.py
+++ clusters.py
@@ -75,17 +75,3 @@ class Clusters:
             self._particle_count -= cluster_size
         else:
             raise ValueError(f'no cluster of size {cluster_size} in group of clusters')
-
-
-# examples
-clusters_a = Clusters.create_monomers(100)
-print(f'Original (Size {clusters_a.particle_count()}):\n', clusters_a)
-successful_collisions = clusters_a.collide(50)
-print(f'After {successful_collisions} collisions (Size {clusters_a.particle_count()}):\n', clusters_a)
-print()
-
-clusters_b = Clusters({2: 100000})
-print(f'Original (Size {clusters_b.particle_count()}):\n', clusters_b)
-successful_collisions = clusters_b.collide(50000, 1)
-print(f'After {successful_collisions} collisions (Size {clusters_b.particle_count()}):\n', clusters_b)
-print('Size:', clusters_b.particle_count())
