1075f75279d21fc9f86a8c090315ed64cae4d6d3
Matthew Rakauskas <matthew.rakauskas@scranton.edu>
2025-06-06 13:26:06 -0400

    Refactor tracking

--- plots.py
+++ plots.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import make_smoothing_spline
 
 import numpy as np
-
+'''
 clusters = Clusters({2: 100000})
 clusters.collide(100000, 1)
 
 data1 = clusters.cluster_list()
 plt.hist(data1)
 plt.show()
+'''
 
 def max_cluster_size(clusters):
     c_keys = clusters.cluster_counts().keys()
@@ -18,16 +19,20 @@ def max_cluster_size(clusters):
     else:
         return 0
 
-clusters2 = Clusters({3: 10000, 1: 50000})
-tracker2 = ClusterTracker(clusters2)
-tracker2.add_tracker('cluster count', lambda c: c.cluster_count(1), lambda c, i: i / tracker2._initial_cluster_count)
-tracker2.add_tracker('max cluster size', lambda c: max_cluster_size(c), lambda c, i: i / c.particle_count())
+clusters = Clusters({3: 10000, 1: 50000})
+tracker = ClusterTracker(clusters)
+tracker.add_tracker('cluster count', lambda c: c.cluster_count(1), lambda c, i: i / tracker._initial_cluster_count)
+tracker.add_tracker('max cluster size', lambda c: max_cluster_size(c), lambda c, i: i / c.particle_count())
 
-tracker2.run(removals=2)
+tracker.run(100, removals=2)
+plt.hist(clusters.cluster_list())
+plt.show()
 
-tracker2.plot_against_collisions('cluster count')
-tracker2.plot_against_collisions('max cluster size')
+tracker.run(removals=2)
+tracker.plot_against_collisions('cluster count')
+tracker.plot_against_collisions('max cluster size')
 
+'''
 spl =  make_smoothing_spline([i / tracker2._collisions for i in range(tracker2._collisions)], tracker2._trackers['max cluster size'][1], lam=0)
 x_cont = np.arange(0, 1, 0.01)
 plt.plot(x_cont, spl(x_cont))
@@ -35,3 +40,4 @@ plt.plot(x_cont, x_cont, '-.')
 diff_curve = x_cont - spl(x_cont)
 plt.plot(x_cont, diff_curve)
 plt.show()
+'''
