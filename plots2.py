50970c0d3ec7a286bcb7c9365b445f7f582faace
MWR27 <64335495+MWR27@users.noreply.github.com>
2025-07-08 23:44:41 -0400

    Add log scaling

--- plots2.py
+++ plots2.py
@@ -1,7 +1,8 @@
 from clusters import Clusters, ClusterTracker
 import matplotlib.pyplot as plt
 import numpy as np
-
+import math
+from scipy.optimize import curve_fit
 
 # simulation
 clusters = Clusters({2: 3000000})
@@ -9,15 +10,21 @@ clusters.collide(times=2000000, removals=1)
 
 # plotting
 sizes = clusters.cluster_sizes(50)
+print(sizes)
 counts = [clusters.cluster_count(size) / clusters.initial_cluster_count() for size in sizes]
-plt.plot(sizes, counts, zorder=2)
+plt.xscale('log')
+plt.yscale('log')
+plt.scatter([i for i in clusters.cluster_sizes(50)], counts, zorder=1)
 plt.xlabel('Cluster Size')
 plt.ylabel('Cluster Count (Scaled)')
-
-x_cont = np.arange(2, 50, 1)
-decay_func = x_cont ** (-5/2)
-plt.plot(x_cont, decay_func, zorder=1)
-plt.legend(['Cluster Count (Scaled)', 'Decay Rate'])
+popt, pcov = curve_fit(lambda t, a, b: a * t ** b, sizes, counts)
+a = popt[0]
+b = popt[1]
+print(a, b)
+x_cont = np.arange(2, 51, 1)
+y_fitted = a * (x_cont ** b)
+plt.plot(x_cont, y_fitted, zorder=2, color='red')
+plt.legend(['Cluster Count (Scaled)', f'$y={a}x^{b}$'])
 
 plt.show()
 print("Done!")
