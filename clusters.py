62f2515e2119785802f3d8f65f441f4739304b5a
MWR27 <64335495+MWR27@users.noreply.github.com>
2025-07-18 10:30:23 -0400

    Modify possible_reactions and add function to find min steps to produce cluster

--- clusters.py
+++ clusters.py
@@ -220,7 +220,7 @@ def possible_reactions(cluster_counts: dict, removals=0) -> set:
     for i in range(len(sizes)):
         for j in range(i, len(sizes)):
             if (i != j or cluster_counts[sizes[i]] >= 2) and sizes[i] + sizes[j] - removals >= 0:
-                possible_reactions.add((sizes[i], sizes[j]))
+                possible_reactions.add((sizes[i], sizes[j], sizes[i] + sizes[j] - removals))
     return possible_reactions
 
 def is_valid_combo(t: tuple, d: dict, particle_count: int) -> bool:
@@ -228,3 +228,40 @@ def is_valid_combo(t: tuple, d: dict, particle_count: int) -> bool:
         return d[t[0]] >= 2 and t[0] + t[1] <= particle_count
     else:
         return t[0] + t[1] <= particle_count
+
+# incomplete
+def min_steps_to_produce(target_value, cluster_counts: dict, removals=0) -> int:
+    return _change_making(cluster_counts.keys(), target_value)
+
+# copied from Wikipedia
+def _get_change_making_matrix(set_of_coins, r: int):
+    m = [[0 for _ in range(r + 1)] for _ in range(len(set_of_coins) + 1)]
+    for i in range(1, r + 1):
+        m[0][i] = math.inf  # By default there is no way of making change
+    return m
+
+def _change_making(coins, n: int):
+    """This function assumes that all coins are available infinitely.
+    if coins are only to be used once, change m[c][r - coin] to m[c - 1][r - coin].
+    n is the number to obtain with the fewest coins.
+    coins is a list or tuple with the available denominations.
+    """
+    m = _get_change_making_matrix(coins, n)
+    for c, coin in enumerate(coins, 1):
+        for r in range(1, n + 1):
+            # Just use the coin
+            if coin == r:
+                m[c][r] = 1
+            # coin cannot be included.
+            # Use the previous solution for making r,
+            # excluding coin
+            elif coin > r:
+                m[c][r] = m[c - 1][r]
+            # coin can be used.
+            # Decide which one of the following solutions is the best:
+            # 1. Using the previous solution for making r (without using coin).
+            # 2. Using the previous solution for making r - coin (without
+            #      using coin) plus this 1 extra coin.
+            else:
+                m[c][r] = min(m[c - 1][r], 1 + m[c][r - coin])
+    return m[-1][-1]
