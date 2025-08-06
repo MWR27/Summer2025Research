d02a92514ad5ccf48f1749efe0e54a1de1c730af
Matthew Rakauskas <matthew.rakauskas@scranton.edu>
2025-08-06 12:32:10 -0400

    Change initial conditions

--- poster.py
+++ poster.py
@@ -4,14 +4,14 @@ import numpy as np
 import math
 
 # simulation
-clusters = Clusters({1: 1000000, 2: 1000000, 3: 1000000})
+clusters = Clusters({2: 3000000})
 for i in range(3):
-    clusters.collide(times=666666, removals=2)
+    clusters.collide(times=700000, removals=1)
     # plotting
-    sizes = clusters.cluster_sizes(50)
+    sizes = clusters.cluster_sizes(10)
     count_fracs = [clusters.cluster_count(size) / clusters.initial_cluster_count() for size in sizes]
 
-    plt.scatter(sizes, count_fracs, zorder=1, label=f'{clusters.collision_count()} collisions')
+    plt.plot(sizes, count_fracs, zorder=1, label=f'{clusters.collision_count()} collisions')
 plt.xlabel('cluster size')
 plt.ylabel('cluster fraction')
 
